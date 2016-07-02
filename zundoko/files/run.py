c530cdf64af3094b0d16368c8c5c99e9ca6085db
Yuji Oshima <yuji.oshima0x3fd@gmail.com>
2016-07-03 02:26:56 +0900

    add zundoko docker image
    Signed-off-by: Yuji Oshima <yuji.oshima0x3fd@gmail.com>

---
+++ zundoko/files/run.py
@@ -0,0 +1,38 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import socket
+
+hostname = socket.gethostname()
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        uri = self.path
+        body = ""
+        if uri == "/zun":
+            body = b"zun"
+        elif uri == "/doko":
+            body = b"doko"
+        elif uri == "/kiyoshi":
+            body = b"__kiyoshi__"
+        else :
+            body = judge_reponce(hostname).encode('utf-8')
+
+        self.send_response(200)
+        self.end_headers()
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.send_header('Content-length', len(body))
+        self.wfile.write(body)
+
+def judge_reponce(cid):
+    intid = int(hostname, 16)
+    if intid % 3 == 0:
+        return "zun"
+    elif intid % 3 == 1:
+        return "doko"
+    else:
+        return "**kiyoshi**"
+
+host = '0.0.0.0'
+port = 8000
+httpd = HTTPServer((host, port), MyHandler)
+print('serving at port', port)
+httpd.serve_forever()
